91ca4b3ca983aec9a236889f0f54114e6b123d78
Tomislav <Tomislavvidovic729@gmail.com>
2023-11-27 16:05:28 +0100

    answ

--- script.js
+++ script.js
@@ -13,12 +13,15 @@ function quiz(data) {
 
     var contentWrapper = document.getElementsByClassName("content-wrapper");
     let questionWrapper = document.getElementById("question");
-    var answerWrapper = document.getElementsByClassName("answers");
-
+    var answerWrapper = document.getElementsByClassName("answers")[0];
     var rewardDiv = document.getElementById("right-conent");
 
 
+
     questionWrapper.innerText = data.questions[0].question;//ubacili smo pitanje iz question.json u question div
+    answerWrapper.innerHTML = `<div class="ans"> ${data.questions[0].answer[0]}</div><div class="ans"> ${data.questions[0].answer[1]}</div>
+    <div class="ans"> ${data.questions[0].answer[2]}</div>
+    <div class="ans"> ${data.questions[0].answer[3]}</div>`
 
 }
 
