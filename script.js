89bc089ef1c3aca9c81eca0ac33404666b1df827
Tomislav <Tomislavvidovic729@gmail.com>
2023-11-27 15:44:29 +0100

    question in div

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 //
 
-questions = {
-    question: "in the sport of JiuJitsu, an omoplata is an attack to the:",
+fetch("question.json")
+    .then(response => response.json())
+    .then(data => quiz(data))
 
-    answer: ["arm", "neck", "leg", "shoulder"],
 
-    correctAns: 3
+function quiz(data) {
+    // console.log(data)
 
-};
+    // var questionNum = 0;
+    // var moneyTrack = 0;
+
+    var contentWrapper = document.getElementsByClassName("content-wrapper");
+    let questionWrapper = document.getElementById("question");
+    var answerWrapper = document.getElementsByClassName("answers");
+
+    var rewardDiv = document.getElementById("right-conent");
+
+
+    questionWrapper.innerText = data.questions[0].question;//ubacili smo pitanje iz question.json u question div
+
+}
+
+
+
+
 
