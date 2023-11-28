5e6d872ceb7d08e564d56a0b2e555fd18cf10e77
Tomislav <Tomislavvidovic729@gmail.com>
2023-11-28 15:46:31 +0100

    .

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //
 
+
 fetch("question.json")
     .then(response => response.json())
     .then(data => quiz(data))
@@ -7,18 +8,19 @@ fetch("question.json")
 
 function quiz(data) {
     // console.log(data)
-
     let questionNum = 0;
-    // var moneyTrack = 0;
+    var moneyTrack = 0;
 
-    var contentWrapper = document.getElementsByClassName("content-wrapper");
+
+    // var contentWrapper = document.getElementsByClassName("content-wrapper");
     let questionWrapper = document.getElementById("question");
     var answerWrapper = document.querySelector(".answers");
     var rewardDiv = document.getElementById("right-conent");
-    let allAnswers = data.questions[0].answer//to je array
-    let correctAns = data.questions[0].correctAns;
+    let allQuestions = data.questions[questionNum].question;
+    let allAnswers = data.questions[questionNum].answer//to je array
+    let correctAns = data.questions[questionNum].correctAns;
 
-    odgovor0 = allAnswers[0];
+
     const [answer0, answer1, answer2, answer3] = [
         allAnswers[0],
         allAnswers[1],
@@ -26,8 +28,8 @@ function quiz(data) {
         allAnswers[3]
 
     ];
-
-
+    //prvo pitanje
+    questionWrapper.innerText = data.questions[questionNum].question;
 
 
     for (let i = 0; i < allAnswers.length; i++) {
@@ -36,66 +38,66 @@ function quiz(data) {
     <div class="ans"> ${answer3}</div>`
     }//iteracija kroz pitanja i stavlja ih u container
 
+    // console.log(allQuestions);
 
-
-    // Dodajte event listener za svaki odgovor
+    // event listener za svaki odgovor
     let ans = document.querySelectorAll(".ans");
-
 
     ans.forEach((ans, index) => {
         ans.addEventListener("click", () => checkCorrect(index, correctAns));
+
     });
 
-
-
-
-
-
-
-
-
-
-    function checkCorrect(userAnswerIndex, correctAns) {
-        if (userAnswerIndex == correctAns) {
+    //akko uspijem povećati index onda ce correctAns++
+    function checkCorrect(userAnswerIndex, index) {
+        console.log(index);
+        if (userAnswerIndex === index) {
             console.log("Točan odgovor!");
             questionNum++
-            console.log(questionNum)
+            moneyTrack++
+
+            correctAns = data.questions[questionNum].correctAns;//moram ga opet ovdje staviti da se ucita
+
+
+
+            // ans[userAnswerIndex].style.backgroundColor = "green";
+            questionWrapper.innerText = data.questions[questionNum].question;//opet ucitavam pitanja jer je questionNum++
+
+
+
+
+            //  delay 
+            /* setTimeout(() => {
+              ans[userAnswerIndex].style.backgroundColor = "";
+            }, 900);*/
+
+
         } else {
             console.log("Pogrešan odgovor!");
+            // ans[userAnswerIndex].style.backgroundColor = "orange";
 
 
-        };
 
-        questionWrapper.innerText = data.questions[questionNum].question;//ubacili smo pitanje iz question.json u question div
+            /*// delay 
+            setTimeout(() => {
+                questionWrapper.innerText = data.questions[questionNum].question;
+                ans.forEach(answer => {
+                    // ans[userAnswerIndex].style.backgroundColor = "red";
+                });
+            }, 200);*/
 
+
+        }
+        console.log(questionNum, moneyTrack)
+        // console.log("tocan odgovor je ", correctAns);
+        //console.log("broj pitanja je ", questionNum);
+        //console.log(correctAns[questionNum]);
+        // console.log(ans[userAnswerIndex]);
+        // console.log(userAnswerIndex);
     }
-
-
-
-
-
-
-
-
-
-
-    /*
-        let answerDivs = document.querySelectorAll(".ans");
-        answerDivs.forEach((answerDiv, index) => {
-            console.log("Element:", answerDiv); // Trenutni element
-            console.log("Indeks:", index);      // Indeks trenutnog elementa
-        });*/
 }
 
 
 
-
-
-
-
-
-
-
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
